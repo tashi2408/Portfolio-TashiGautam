a012c16a7eb3c0d1d3dda408085684917cb0103a
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-09-03 02:20:46 +0530

    components and pages added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,50 @@
 import React from "react"
 import Layout from "../components/Layout"
 
+import * as styles from "../styles/about.module.scss"
+
+import { AiOutlineLogin } from "react-icons/ai"
+import { IoCheckmark } from "react-icons/io5"
+import Me from "../../static/2.jpg"
+
 export default function About() {
   return (
     <Layout>
       <div>
-        <h1>About Page</h1>
+        <section className={styles.intro}>
+          <div className={styles.intro__left}>
+            <img src={Me} />
+          </div>
+          <div className={styles.intro__right}>
+            <h1>Tashi Gautam</h1>
+            <p>
+              My name is John Smith. I am a web designer and developer.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut laboagna aliqua. Quis ipsum suspendisse
+              ultrices gravida. Risus commodo viverra maecenas accumsan lacus
+              vel facilisis.
+            </p>
+            <div className={styles.btn}>
+              <div className={styles.button}>
+                <input id="button" type="checkbox" />
+                <label for="button">
+                  <div className={styles.button_inner}>
+                    <AiOutlineLogin className={styles.l} />
+                    <span className={styles.t}>My Resume</span>
+                    <span>
+                      <IoCheckmark className={styles.tick} />
+                    </span>
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.skills}>
+          <div className={styles.skills_left}></div>
+          <div className={styles.skills_right}></div>
+        </section>
       </div>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,108 @@
+import { Link } from "@reach/router"
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.scss"
+import Projects from "./projects.json"
+import Typewriter from "typewriter-effect"
+import SVG from "../components/SVG"
+import SVGg from "../components/SVGg"
+import ParallaxProjects from "../components/ParallaxProjects"
+import Heading from "../components/Heading"
 
 export default function Home() {
   return (
-    <Layout>
-      <section>
-        <h2> hello</h2>
-        <h2>world</h2>
-      </section>
+    <Layout className={styles.hide}>
+      <div className={styles.hide}>
+        <div className={styles.home}>
+          <section className={`${styles.basecontainer} ${styles.group1}`}>
+            <div className={`${styles.container} ${styles.container_fore}`}>
+              <div className={styles.headtitle}>
+                <h1>Hello!</h1>
+                <h3>Nice to see you here.</h3>
+              </div>
+              <div className={styles.image}>
+                <SVG />
+              </div>
+              <div className={styles.image1}>
+                <SVGg />
+              </div>
+              <div className={styles.image2}>
+                <SVG />
+              </div>
+              <div className={styles.image3}>
+                <SVGg />
+              </div>
+              <div className={styles.image4}>
+                <SVG />
+              </div>
+              <div className={styles.image5}>
+                <SVGg />
+              </div>
+              <div className={styles.image6}>
+                <SVG />
+              </div>
+              <div className={styles.image7}>
+                <SVGg />
+              </div>
+              <div className={styles.image8}>
+                <SVGg />
+              </div>
+            </div>
+            <div
+              className={`${styles.container} ${styles.container_base}`}
+            ></div>
+            <div
+              className={`${styles.container} ${styles.container_back}`}
+            ></div>
+          </section>
+          <section className={`${styles.basecontainer} ${styles.group2}`}>
+            <div className={`${styles.container} ${styles.container_fore}`}>
+              <div className={styles.headtitle}>
+                <h1>I'm Tashi Gautam</h1>
+                <Typewriter
+                  onInit={typewriter => {
+                    typewriter
+                      .typeString(" Web Developer")
+                      .pause(2000)
+                      .deleteAll()
+                      .typeString(" Machine Learning Enthusiat")
+                      .pause(2000)
+                      .deleteAll()
+                      .typeString(
+                        " Web Developer and Machine Learning Enthusiat"
+                      )
+                      .start()
+                  }}
+                />
+              </div>
+              <div className={styles.image1}>
+                <SVG />
+              </div>
+              <div className={styles.image2}>
+                <SVGg />
+              </div>
+              <div className={styles.image3}>
+                <SVG />
+              </div>
+              <div className={styles.image4}>
+                <SVGg />
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className={styles.project_head}>
+              <Heading text="my projects" />
+            </div>
+
+            <ParallaxProjects />
+          </section>
+          <footer>
+            <p>Cpoyright 2021 Tashi Gautam</p>
+          </footer>
+        </div>
+      </div>
     </Layout>
   )
 }
+
+/*  */
