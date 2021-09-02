a1a989dabaa876a7721bca8b843a623dcde6c188
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-09-03 02:00:30 +0530

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,37 @@ import React from "react"
 
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Logo</h1>
-      <div className="nav-links">
-        <Link to="/">Home</Link>
+    <div className="nav_bar">
+      <input
+        class="menu-icon"
+        type="checkbox"
+        id="menu-icon"
+        name="menu-icon"
+      />
+      <label for="menu-icon"></label>
+      <nav class="nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Conatct Me</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+{
+  /* <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Conatct Me</Link>
-        <Link to="/projects">Projects</Link>
-      </div>
-    </nav>
-  )
+        <Link to="/projects">Projects</Link> */
 }
