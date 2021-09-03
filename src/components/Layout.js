57208ea6c605c75ab719855898aeb01e57cc9b64
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-09-04 02:47:11 +0530

    sticky socials added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import Navbar from "./Navbar"
 import "../styles/global.scss"
 import { lightTheme, darkTheme } from "./Theme"
+import SocialIcons from "./SocialIcons"
 
 export default function Layout({ children }) {
   const [theme, setTheme] = useState("light")
@@ -17,6 +18,7 @@ export default function Layout({ children }) {
     <div className="layout" style={theme === "light" ? lightTheme : darkTheme}>
       <div className="layout-content">
         <Navbar toogleTheme={toggleTheme} />
+        <SocialIcons />
         <div className="content">{children}</div>
       </div>
     </div>
