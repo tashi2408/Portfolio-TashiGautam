92297b79aeacf1b4b7dfc523d574800256bd400d
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-09-07 03:44:16 +0530

    error 404 pages done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,27 @@
 import React from "react"
-import Layout from "../components/Layout"
+import * as styles from "../styles/404style.module.scss"
 
 export default function NotFound() {
   return (
-    <Layout>
-      <div>
-        <h1>404</h1>
-        <p>Sorry not found!</p>
+    <div className={styles.error}>
+      <h1>404</h1>
+      <p>Sorry not found!</p>
+      <div className={styles.ghost_canvas}>
+        <div className={styles.ghost_body}>
+          <div className={styles.ghost_body_copy}></div>
+          <div className={styles.ghost_head}>
+            <div className={styles.eye_left}></div>
+            <div className={styles.eye_right}></div>
+            <div className={styles.mouth}></div>
+          </div>
+          <div className={styles.down_bubble}>
+            <div className={styles.bubble_1}></div>
+            <div className={styles.bubble_2}></div>
+            <div className={styles.bubble_3}></div>
+            <div className={styles.bubble_4}></div>
+          </div>
+        </div>
       </div>
-    </Layout>
+    </div>
   )
 }
