72b165d3dfda63e7ec38400104e54cc721709369
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-09-07 03:43:56 +0530

    added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [`gatsby-plugin-sass`],
+  siteMetadata: {
+    title: "Tashi Gautam",
+    description: "I'm a Web Developer and a Machine Learning Enthusiat",
+    copyright: "This website is copyright 2021 Tashi Gautam",
+  },
 }
